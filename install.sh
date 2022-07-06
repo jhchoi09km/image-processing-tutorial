4a5aa146247a1ca77a03d2468f8e3081eff3dc03
JanghoonChoi <jhchoi09@kookmin.ac.kr>
2022-07-06 13:22:09 +0900

    init

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libatlas-base-dev libopenblas-dev libblas-dev python3-dev python3-yaml python3-setuptools
+pip3 install opencv-python
+pip3 install -U numpy
+wget https://github.com/jhchoi09/PyTorch-and-Vision-for-Raspberry-Pi-4B/raw/master/torch-1.8.0a0%2B56b43f4-cp37-cp37m-linux_armv7l.whl
+wget https://github.com/jhchoi09/PyTorch-and-Vision-for-Raspberry-Pi-4B/raw/master/torchvision-0.9.0a0%2B8fb5838-cp37-cp37m-linux_armv7l.whl
+pip3 install torch*.whl
