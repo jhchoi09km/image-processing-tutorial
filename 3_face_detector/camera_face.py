53d0fe5efb279c250e210ae2e0cb1dce8d638fd2
JanghoonChoi <jhchoi09@kookmin.ac.kr>
2022-07-25 12:43:36 +0900

    faces complete

--- 3_face_detector/camera_face.py
+++ 3_face_detector/camera_face.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 
 cam = cv2.VideoCapture(0)
+interval = 10
 
 while True:
     _, img = cam.read()
@@ -9,12 +10,18 @@ while True:
 
     haar_cascade = cv2.CascadeClassifier('haar_face.xml')
     faces_rect = haar_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=1)
-    x,y,w,h = faces_rect[0]
 
-    cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
+    if len(faces_rect)<1:
+        cv2.imshow('video', img)
+        cv2.waitKey(interval)
+        continue
+
+    for i in range(len(faces_rect)):
+        x,y,w,h = faces_rect[i]
+        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), thickness=2)
 
     cv2.imshow('video', img)
-    cv2.waitKey(100)
+    cv2.waitKey(interval)
 
 
 cam.release()
