f70f354146b7226b58a1e927d61b6ea119bc6f1c
JanghoonChoi <jhchoi09@kookmin.ac.kr>
2022-07-27 10:31:56 +0900

    resize

--- 3_face_detector/camera_face.py
+++ 3_face_detector/camera_face.py
@@ -6,6 +6,7 @@ interval = 1
 
 while True:
     _, img = cam.read()
+    img = cv2.resize(img, dsize=(0,0), fx=0.5, fy=0.5)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     haar_cascade = cv2.CascadeClassifier('haar_face.xml')
